571cec54c8468a93a0c2f5f0e4c80a0cff49c091
John Remboldt and Robin Sola <robin@>
2017-10-25 18:43:27 -0700

    add radio buttons for add, sub, mul, div.

--- js/scripts.js
+++ js/scripts.js
@@ -19,32 +19,44 @@ var divide = function(number1, number2) {
 // User interface (or front-end) logic:
 
 $(document).ready(function() {
-    $("button#add").click(function(event) {
+    $("form#calc").submit(function() {
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-    $("button#subtract").click(function(event) {
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-    $("button#multiply").click(function(event) {
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-    event.preventDefault();
-  });
-    $("button#divide").click(function(event) {
-    var number1 = parseInt($("#input1").val());
-    var number2 = parseInt($("#input2").val());
-    var result = divide(number1, number2);
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
     event.preventDefault();
   });
 });
+
+
+
+  //   $("button#subtract").click(function(event) {
+  //   var number1 = parseInt($("#input1").val());
+  //   var number2 = parseInt($("#input2").val());
+  //   var result = subtract(number1, number2);
+  //   $("#output").text(result);
+  //   event.preventDefault();
+  // });
+  //   $("button#multiply").click(function(event) {
+  //   var number1 = parseInt($("#input1").val());
+  //   var number2 = parseInt($("#input2").val());
+  //   var result = multiply(number1, number2);
+  //   $("#output").text(result);
+  //   event.preventDefault();
+  // });
+  //   $("button#divide").click(function(event) {
+  //   var number1 = parseInt($("#input1").val());
+  //   var number2 = parseInt($("#input2").val());
+  //   var result = divide(number1, number2);
+  //   $("#output").text(result);
+  //   event.preventDefault();
